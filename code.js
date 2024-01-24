57d73177785aebaae0ca0bef8031bf80c3508c28
Noah Mulvaney <mulvaney.np@gmail.com>
2024-01-24 09:17:50 -0700

    Fix code.js

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
+//code.js
+//COSC3020 Sum Exercise 
+//Noah Mulvaney, nmulvane@uwyo.edu
+//24 Jan 2024
+
 function sum(a) {
     var sum = a[0];
-    for(var i = 0; i < a.length; i++) {
+    for(var i = 1; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
